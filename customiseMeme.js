e26919aa20966af971ceaa1b577ca97539d0e672
TheMrCake <antonlichota@gmail.com>
2024-04-28 20:35:12 +0100

    Added Gitignore, and the ability to upload image

---
+++ customiseMeme.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById('memeCanvas');
+const ctx = canvas.getContext('2d');
+let dragging = false;
+let textX1 = 50,
+    textY1 = 50;
+let textX2 = 50,
+    textY2 = 100;
+
+console.log("hello");
+
+function loadUserImage() {
+    // Get User input file
+    var file = document.getElementById('userImageInput').files[0];
+    // If file exists...
+    if (file) {
+        // Create new file reader
+        var reader = new FileReader();
+        // Assign function on 'onload event'
+        reader.onload = function(e) {
+            loadTemplate(e.target.result);
+            generateMeme();
+        };
+        // Read file
+        reader.readAsDataURL(file);
+    }
+}
+
+function loadTemplate(src) {
+    // Assign the source of the selected template to the global variable
+    const img = new Image();
+    img.onload = function() {
+        let scale = Math.min(500 / img.width, 500 / img.height);
+        let width = img.width * scale;
+        let height = img.height * scale;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, width, height);
+        generateMeme(); // Call generateMeme after the image is drawn
+    };
+    img.src = src;
+}
+
+function generateMeme() {
+    const img = new Image();
+    img.onload = function () {
+        // Set max size by scale.
+        let scale = Math.min(500 / img.width, 500 / img.height);
+        let width = img.width * scale;
+        let height = img.height * scale;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.drawImage(img, 0, 0, width, height);
+    };
+    img.src = document.getElementById('userImageInput').files.length > 0 ? URL.createObjectURL(document.getElementById('userImageInput').files[0]) : selectedTemplateSrc;
+}
